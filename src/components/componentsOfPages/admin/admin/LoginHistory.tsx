14968d587db9259ff9042c9f214b0e051a7cf869
Гор <gor@MacBook-Pro-Gor-2.local>
2024-12-26 23:07:15 +0300

    Adding loginHistory component for admin page

---
+++ src/components/componentsOfPages/admin/admin/LoginHistory.tsx
@@ -0,0 +1,68 @@
+import { FC } from "react";
+import HeadingOfSection from "../../../UI/text/HeadingOfSection";
+import H2 from "../../../UI/text/H2";
+import Subtitle from "../../../UI/text/Subtitle";
+import H4 from "../../../UI/text/H4";
+import TextMain from "../../../UI/text/TextMain";
+
+
+const LoginHistory: FC = () => {
+    return (
+        <>
+            <section className="mt-[130px] T:mt-[100px] TS:mt-[70px] px-[12%] L:px-[20px] P:px-[10px]">
+                <div>
+                    <div>
+                        <HeadingOfSection className="text-center">История входов</HeadingOfSection>
+                    </div>
+                    <div>
+                        <H2 className="text-center mt-[8px] mb-[16px]">Все устройства и время входа</H2>
+                    </div>
+                    <div className="w-3/4 TS:w-full mx-auto">
+                        <Subtitle className="text-center">Здесь представлена база данных всех менеджеров, представлены последнее время входа, устройства, с которых вошли</Subtitle>
+                    </div>
+                </div>
+                <div className="mt-[40px] overflow-x-auto">
+                    <div className="flex flex-col gap-4 mt-6">
+                        <H4>Менеджер 1 | Имя</H4>
+                        <div className="flex flex-col gap-1">
+                            <TextMain>device</TextMain>
+                            <TextMain>ip address</TextMain>
+                            <TextMain>last login date</TextMain>
+                        </div>
+                        <div className="flex flex-col gap-1">
+                            <TextMain>device</TextMain>
+                            <TextMain>ip address</TextMain>
+                            <TextMain>last login date</TextMain>
+                        </div>
+                        <div className="flex flex-col gap-1">
+                            <TextMain>device</TextMain>
+                            <TextMain>ip address</TextMain>
+                            <TextMain>last login date</TextMain>
+                        </div>
+                    </div>
+
+                    <div className="flex flex-col gap-4 mt-6">
+                        <H4>Менеджер 1 | Имя</H4>
+                        <div className="flex flex-col gap-1">
+                            <TextMain>device</TextMain>
+                            <TextMain>ip address</TextMain>
+                            <TextMain>last login date</TextMain>
+                        </div>
+                        <div className="flex flex-col gap-1">
+                            <TextMain>device</TextMain>
+                            <TextMain>ip address</TextMain>
+                            <TextMain>last login date</TextMain>
+                        </div>
+                        <div className="flex flex-col gap-1">
+                            <TextMain>device</TextMain>
+                            <TextMain>ip address</TextMain>
+                            <TextMain>last login date</TextMain>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    );
+}
+
+export default LoginHistory;
--- src/pages/admin/admin/Admin.tsx
+++ src/pages/admin/admin/Admin.tsx
@@ -2,6 +2,7 @@ import { FC, useEffect, useState } from "react";
 import AllUsersSection from "../../../components/componentsOfPages/admin/admin/AllUsersSection";
 import TodayUsersSection from "../../../components/componentsOfPages/admin/admin/TodayUsersSection";
 import { UserPhone } from "../../../models/UserPhone";
+import LoginHistory from "../../../components/componentsOfPages/admin/admin/LoginHistory";
 
 const Admin: FC = () => {
     let [userPhones, setUserPhones] = useState<UserPhone[]>();
@@ -17,6 +18,7 @@ const Admin: FC = () => {
             <main className="pb-52">
                 <AllUsersSection/>
                 <TodayUsersSection/>
+                <LoginHistory/>
             </main>
         </>
     );
